c5539443233cb310ecbc00e7ab9bb316f534bb30
dj-d <daniele.albanese98@gmail.com>
2021-07-16 00:18:52 +0200

    Reset all

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import React from "react";
-import {Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator} from '@material-ui/lab';
 
 function App() {
-  return (
-    <div>
-        <Timeline align="alternate">
-            <TimelineItem>
-                <TimelineSeparator>
-                    <TimelineDot />
-                    <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>Eat</TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-                <TimelineSeparator>
-                    <TimelineDot />
-                    <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>Code</TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-                <TimelineSeparator>
-                    <TimelineDot />
-                    <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>Sleep</TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-                <TimelineSeparator>
-                    <TimelineDot />
-                </TimelineSeparator>
-                <TimelineContent>Repeat</TimelineContent>
-            </TimelineItem>
-        </Timeline>
-    </div>
-  );
+	return (
+		<div style={{marginTop: "2em"}}>
+		</div>
+	);
 }
 
 export default App;
