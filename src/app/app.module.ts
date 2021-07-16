5a6ea495ae7bca86b833d662fa0196181e27232f
Marco Russodivito <russodivito.marco@gmail.com>
2021-07-16 12:02:46 +0200

    bar plot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
 import {TimelineModule} from "primeng/timeline";
+import {ChartModule} from "primeng/chart";
+import {NgxEchartsModule} from 'ngx-echarts';
+import { HttpClientModule } from '@angular/common/http';
+
+export function load_echarts() {
+    return import('echarts');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+    ],
     imports: [
         BrowserModule,
-        TimelineModule
+        TimelineModule,
+        ChartModule,
+        NgxEchartsModule.forRoot({echarts: load_echarts}),
+        HttpClientModule
     ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,23 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import {TimelineModule} from 'primeng/timeline';
+import {Component, OnInit} from '@angular/core';
 import {PrimeIcons} from 'primeng/api';
+import {EChartsOption} from "echarts";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  events: any;
 
-  constructor() { }
+    public optionBar: EChartsOption = {
+        title: {
+            text: 'PokéMOL'
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        legend: {
+            data: ['SquadraA', 'SquadraB', 'SquadraC']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'value',
+            boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+            type: 'category',
+            data: ['SquadraA', 'SquadraB', 'SquadraC']
+        },
+        series: [
+            {
+                name: 'Squadre',
+                type: 'bar',
+                data: [0, 0, 0]
+            }
+        ]
+    };
 
-  ngOnInit() {
-    this.events = [
-      {status: 'Ordered', date: '15/10/2020 10:30', icon: PrimeIcons.SHOPPING_CART, color: '#9C27B0', image: 'game-controller.jpg'},
-      {status: 'Processing', date: '15/10/2020 14:00', icon: PrimeIcons.COG, color: '#673AB7'},
-      {status: 'Shipped', date: '15/10/2020 16:15', icon: PrimeIcons.ENVELOPE, color: '#FF9800'},
-      {status: 'Delivered', date: '16/10/2020 10:00', icon: PrimeIcons.CHECK, color: '#607D8B'}
-    ];
-  }
+    public aaaa: EChartsOption = {
+        title: {
+            text: 'aaa',
+        }
+    };
+
+    constructor(private http: HttpClient) {
+    }
+
+    ngOnInit() {
+        this.fetch();
+    }
+
+    public fetch() {
+        setInterval(() => {
+            this.http.get('http://193.205.108.4:3000/api/dashboard/teams')
+                .subscribe((a) => {
+                    let schif = a as Array<any>;
+                    this.aaaa = {
+                        series: {
+                            name: 'Squadre',
+                            type: 'bar',
+                            data: [schif[0].totalScore, schif[1].totalScore, schif[2].totalScore]
+                        }
+                    }
+                })
+        },1000);
+    }
 }
